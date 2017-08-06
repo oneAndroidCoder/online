ad5a524634f88e5fb71458df44d5436d46cd562e
Administrator <1136189725@qq.com>
2017-08-06 12:41:16 +0800

    1.   整合代码

---
+++ online/src/main/java/android/video/online/ui/user/UserCenterFragment.java
@@ -0,0 +1,19 @@
+package android.video.online.ui.user;
+
+import android.video.online.BasicFragment;
+import android.video.online.R;
+
+/**
+ *
+ */
+public class UserCenterFragment extends BasicFragment {
+    @Override
+    public int setLayoutId() {
+        return R.layout.fragment_user_center;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
